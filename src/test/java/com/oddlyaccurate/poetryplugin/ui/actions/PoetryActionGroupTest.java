f2169b2ae7f01e20a0fa642ea5bb7ac0b67427ac
Kevin Yang <kevin.yang@udemy.com>
2019-04-13 13:24:25 -0700

    Upgrade test library to junit jupiter 5.4.2

--- src/test/java/com/oddlyaccurate/poetryplugin/ui/actions/PoetryActionGroupTest.java
+++ src/test/java/com/oddlyaccurate/poetryplugin/ui/actions/PoetryActionGroupTest.java
@@ -1,9 +1,11 @@
 package com.oddlyaccurate.poetryplugin.ui.actions;
 
-import junit.framework.TestCase;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class PoetryActionGroupTest extends TestCase {
+public class PoetryActionGroupTest {
 
+    @Test
     public void testPoetryActionGroupContents () {
         PoetryActionGroup pag = new PoetryActionGroup();
         assertEquals(pag.getChildren(null).length, 5);
